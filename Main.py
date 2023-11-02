f9f2b3b0a36db46833ec696af1aab3aef5526a67
MrBr00d <nvliet@gmail.com>
2023-11-02 13:31:35 +0100

    Cleanup unneeded code

--- Main.py
+++ Main.py
@@ -1,32 +1,28 @@
 from geopy.geocoders import Nominatim
 import pandas as pd
 
-df = pd.read_csv("Map1.csv")
-nrows = len(df)
+def adress_finder(path):
+    df = pd.read_csv(path)
+    nrows = len(df)
 
-streets_dict = df.to_dict(orient="list")
-# streets_dict
-streets_dict["Straat"]
-streets_dict["Stad"]
-appened_streets = []
-for item in range(nrows):
-    appened_streets.append(streets_dict["Straat"][item] + "," + streets_dict["Stad"][item])
+    streets_dict = df.to_dict(orient="list")
+    appened_streets = []
 
-loc = Nominatim(user_agent="Geopy Library")
-lats = []
-longs = []
-for item in range(0, len(appened_streets)):
-    getLoc = loc.geocode(appened_streets[item])
-    lat = getLoc.latitude
-    long = getLoc.longitude
-    print("Latitude = ", lat)
-    print("Longitude = ", long)
-    print(getLoc.address, "\n")
-    lats.append(lat)
-    longs.append(long)
+    for item in range(nrows):
+        appened_streets.append(streets_dict["Straat"][item] + "," + streets_dict["Stad"][item])
+        
+    loc = Nominatim(user_agent="Geopy Library")
+    lats = []
+    longs = []
+    for item in range(0, len(appened_streets)):
+        getLoc = loc.geocode(appened_streets[item])
+        lat = getLoc.latitude
+        long = getLoc.longitude
+        lats.append(lat)
+        longs.append(long)
+    df["Latitude"] = lats
+    df["Longitude"] = longs
+    df.to_csv("output.csv")
 
-df["Latitude"] = lats
-df["Longitude"] = longs
-
-df.head()
-    
+if __name__ == "__main__":
+    adress_finder(input("Please enter the name of the csv file. This csv file ahs to be in the same fodler as this .py file. "))
