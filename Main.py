ce61a3426711cce9cbdf70b1fa007a599c8dc21c
MrBr00d <nvliet@gmail.com>
2023-11-03 12:02:46 +0100

    final commit branch

--- Main.py
+++ Main.py
@@ -1,29 +1,48 @@
 from geopy.geocoders import Nominatim
 import pandas as pd
 
-def adress_finder(path):
-    df = pd.read_csv(path)
+def adress_finder(path, steetname, housenumber, addition):
+    print("This program makes the following assumption: \n")
+    print("- all locations are in the city of Den Haag. \n")
+    input("Press any button to conttinue...")
+    df = pd.read_csv("Map2.csv", index_col=False)
+    df = df[["straatnaam", "huisnummer", "toevoeging"]]
+    df["plaats"] = "Den Haag"
     nrows = len(df)
 
     streets_dict = df.to_dict(orient="list")
     appened_streets = []
 
     for item in range(nrows):
-        appened_streets.append(streets_dict["Straat"][item] + "," + streets_dict["Stad"][item])
-        
+        if pd.isna(streets_dict["toevoeging"][item]):
+            appened_streets.append(streets_dict["straatnaam"][item] + " " + str(streets_dict["huisnummer"][item]) + ", " + streets_dict["plaats"][item])
+        else:
+            appened_streets.append(streets_dict["straatnaam"][item] + " " + str(streets_dict["huisnummer"][item]) + streets_dict['toevoeging'][item] + ", " + streets_dict["plaats"][item])
+            
     loc = Nominatim(user_agent="Geopy Library")
     lats = []
     longs = []
+    failed = []
+    df2 = pd.DataFrame()
+    df2["Address"] = None
     for item in range(0, len(appened_streets)):
-        getLoc = loc.geocode(appened_streets[item])
-        lat = getLoc.latitude
-        long = getLoc.longitude
-        lats.append(lat)
-        longs.append(long)
-        print(getLoc.address)
-    df["Latitude"] = lats
-    df["Longitude"] = longs
-    df.to_csv("output.csv")
+        try:
+            getLoc = loc.geocode(appened_streets[item])
+            lats.append(getLoc.latitude)
+            longs.append(getLoc.longitude)
+            print(getLoc.address)
+            df2.loc[len(df2)] = [appened_streets[item]]
+        except:
+            failed.append(appened_streets[item])
+    df2["Latitude"] = lats
+    df2["Longitude"] = longs
+    print("The following addresses were not found \n")
+    print(failed)
+    df2.to_csv("output.csv", index=False)
+    input("Press any button to close...")
 
 if __name__ == "__main__":
-    adress_finder(input("Please enter the name of the csv file. This csv file ahs to be in the same fodler as this .py file. "))
+    adress_finder(input("Please enter the name of the csv file. This csv file ahs to be in the same fodler as this .py file. "),
+                   input("Please enter the name of the column containing streetnames. "),
+                   input("Please enter the name of the column containing housenumber. "),
+                   input("Please enter the name of the column containing addition to the housenumber (toevoeging). "))
