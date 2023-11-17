563efedf36dca3295e57be73068cc32b7740906e
MrBr00d <nvliet@gmail.com>
2023-11-17 18:16:52 +0100

    final(ish) product

--- Main.py
+++ Main.py
@@ -1,11 +1,12 @@
 from geopy.geocoders import Nominatim
 import pandas as pd
 import time
+import tkinter as tk
+from tkinter import filedialog
 
 def adress_finder(path, steetname, housenumber, addition, place):
-    print("This program makes the following assumption: \n")
-    print("- all locations are in the city of Den Haag. \n")
-    input("Press any button to conttinue...")
+    start = time.time()
+    print("start geocoding...")
     df = pd.read_csv(path, index_col=False)
     df = df[[steetname, housenumber, addition, place]]
     nrows = len(df)
@@ -14,10 +15,10 @@ def adress_finder(path, steetname, housenumber, addition, place):
     appened_streets = []
 
     for item in range(nrows):
-        if pd.isna(streets_dict["toevoeging"][item]):
-            appened_streets.append(streets_dict["straatnaam"][item] + " " + str(streets_dict["huisnummer"][item]) + ", " + streets_dict["plaats"][item])
+        if pd.isna(streets_dict[addition][item]):
+            appened_streets.append(streets_dict[steetname][item] + " " + str(streets_dict[housenumber][item]) + ", " + streets_dict[place][item])
         else:
-            appened_streets.append(streets_dict["straatnaam"][item] + " " + str(streets_dict["huisnummer"][item]) + streets_dict['toevoeging'][item] + ", " + streets_dict["plaats"][item])
+            appened_streets.append(streets_dict[steetname][item] + " " + str(streets_dict[housenumber][item]) + streets_dict[addition][item] + ", " + streets_dict[place][item])
             
     loc = Nominatim(user_agent="Nick van Vliet")
     lats = []
@@ -25,9 +26,11 @@ def adress_finder(path, steetname, housenumber, addition, place):
     failed = []
     df2 = pd.DataFrame()
     df2["Address"] = None
+    print("DATA OK")
     for item in range(0, len(appened_streets)):
         try:
             getLoc = loc.geocode(appened_streets[item])
+            time.sleep(1)
             lats.append(getLoc.latitude)
             longs.append(getLoc.longitude)
             print(getLoc.address)
@@ -39,14 +42,22 @@ def adress_finder(path, steetname, housenumber, addition, place):
     print("The following addresses were not found \n")
     print(failed)
     df2.to_csv("output.csv", index=False)
-    input("Press any button to close...")
+    end = time.time()
+    print(f"Total runtime: {end-start}")
+
 
 if __name__ == "__main__":
-    start = time.time
-    adress_finder(input("Please enter the name of the csv file. This csv file ahs to be in the same fodler as this .py file "),
-                   input("Please enter the name of the column containing streetnames. "),
-                   input("Please enter the name of the column containing housenumber. "),
-                   input("Please enter the name of the column containing addition to the housenumber (toevoeging). "),
-                   input("Please enter the name of the column containing place. "))
-    end = time.time
-    print(end-start)
+    try:
+        root = tk.Tk()
+        root.withdraw()
+        data_path = filedialog.askopenfilename(filetypes=[("CSV", "*.csv")], title="Select CSV file with address info")
+        root.destroy()
+        adress_finder(data_path,
+                    input("Please enter the name of the column containing streetnames. "),
+                    input("Please enter the name of the column containing housenumber. "),
+                    input("Please enter the name of the column containing addition to the housenumber (toevoeging). "),
+                    input("Please enter the name of the column containing place. "))
+        print("Geocoding done")
+        input('press any button to close')
+    except Exception as e:
+        print(e)
